d1fc66facaa6cce4c0ef32bfc49b2e12483eb5a5
Mari test <23mkovtun@mail.ru>
2023-07-26 11:19:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import datetime
+import application.db.people
+from application.salary import calculate_salary
+
+if __name__ == '__main__':
+    print(datetime.date.today())
+    application.db.people.get_employees()
+    calculate_salary()
+
+
+
+
+
+
+
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
